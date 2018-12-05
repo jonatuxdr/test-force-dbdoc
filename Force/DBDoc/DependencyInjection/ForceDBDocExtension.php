9fe60edf0a1e875011ecc477987b1f0b3a7d824b
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-12-05 11:40:02 +0100

    Initial commit

---
+++ Force/DBDoc/DependencyInjection/ForceDBDocExtension.php
@@ -0,0 +1,12 @@
+<?php
+namespace Force\DBDoc\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ForceDBDocExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {}
+}
---
+++ Force/DBDoc/ForceDbDocBundle.php
@@ -0,0 +1,10 @@
+<?php
+namespace Force\DBDoc;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ForceDbDocBundle extends Bundle
+{
+    
+}
+
